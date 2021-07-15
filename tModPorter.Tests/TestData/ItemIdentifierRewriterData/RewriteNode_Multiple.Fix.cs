c3f6cba28cc3cb3d17dfe4d59ca5bd2e59d70f12
Chik3r <ikervaraalarcon@gmail.com>
2021-07-15 12:08:22 -0400

    add some rewrite tests for ItemIdentifierRewriter

---
+++ tModPorter.Tests/TestData/ItemIdentifierRewriterData/RewriteNode_Multiple.Fix.cs
@@ -0,0 +1,15 @@
+﻿using tModPorter.Tests.GenericClasses;
+
+namespace tModPorter.Tests.TestData.SimpleIdentifierRewriterData
+{
+    public class ItemIdentifier_Multiple
+    {
+        void Method(Item item) { }
+
+        void Method()
+        {
+            Item.field = 0;
+            Item.otherField = 0;
+        }
+    }
+}
---
+++ tModPorter.Tests/TestData/ItemIdentifierRewriterData/RewriteNode_Single.Fix.cs
@@ -0,0 +1,10 @@
+﻿namespace tModPorter.Tests.TestData.SimpleIdentifierRewriterData
+{
+    public class ItemIdentifier_Single
+    {
+        void Method()
+        {
+            Item.field = 0;
+        }
+    }
+}
