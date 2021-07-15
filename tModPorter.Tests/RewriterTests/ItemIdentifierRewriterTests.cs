509d6f244f33c4a973a08156d7e433a2b0d6a532
Chik3r <ikervaraalarcon@gmail.com>
2021-07-15 14:53:02 -0400

    move VisitNode_CheckNodeCount to an abstract class and inherit it

--- tModPorter.Tests/RewriterTests/ItemIdentifierRewriterTests.cs
+++ tModPorter.Tests/RewriterTests/ItemIdentifierRewriterTests.cs
@@ -1,35 +1,17 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using tModPorter.Rewriters;
 using tModPorter.Rewriters.IdentifierRewriters;
-using Xunit;
+using tModPorter.Tests.RewriterTests.SimpleIdentifierTests;
 
 namespace tModPorter.Tests.RewriterTests
 {
-    public class ItemIdentifierRewriterTests
+    public sealed class ItemIdentifierRewriterTest : BaseIdentifierTest
     {
-        [Theory]
-        [InlineData("TestData/ItemIdentifierRewriterData/VisitNode_None.cs", 0)]
-        [InlineData("TestData/ItemIdentifierRewriterData/VisitNode_Single.cs", 1)]
-        [InlineData("TestData/ItemIdentifierRewriterData/VisitNode_Multiple.cs", 2)]
-        public void VisitNode_CheckNodeCount(string inputFile, int numberMatches)
-        {
-            string source = File.ReadAllText(inputFile);
-            Utils.CreateCSharpCompilation(source, "ItemIdentifierTest", out _, out CompilationUnitSyntax root, out SemanticModel model);
-            
-            HashSet<(BaseRewriter rewriter, SyntaxNode originalNode)> nodeSet = new();
-            SimpleIdentifierRewriter rewriter = new ItemIdentifierRewriter(model, null, nodeSet);
-            
-            IEnumerable<SyntaxNode> descendantNodes = root.DescendantNodes();
+        protected override SimpleIdentifierRewriter CreateIdentifierRewriter(SemanticModel model,
+            HashSet<(BaseRewriter rewriter, SyntaxNode originalNode)> nodeSet) => new ItemIdentifierRewriter(model, null, nodeSet);
 
-            foreach (SyntaxNode assigmentNode in descendantNodes)
-                rewriter.VisitNode(assigmentNode);
-            
-            Assert.Equal(numberMatches, nodeSet.Count);
-        }
-        
+        /*
         [Theory]
         [InlineData("TestData/ItemIdentifierRewriterData/RewriteNode_Single.cs", "TestData/ItemIdentifierRewriterData/RewriteNode_Single.Fix.cs")]
         [InlineData("TestData/ItemIdentifierRewriterData/RewriteNode_Multiple.cs", "TestData/ItemIdentifierRewriterData/RewriteNode_Multiple.Fix.cs")]
@@ -51,5 +33,6 @@ namespace tModPorter.Tests.RewriterTests
 
             Assert.Equal(target, root.ToFullString());
         }
+        */
     }
 }
---
+++ tModPorter.Tests/RewriterTests/SimpleIdentifierTests/BaseIdentifierTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using tModPorter.Rewriters;
+using tModPorter.Rewriters.IdentifierRewriters;
+using Xunit;
+
+namespace tModPorter.Tests.RewriterTests.SimpleIdentifierTests
+{
+    public abstract class BaseIdentifierTest
+    {
+        protected abstract SimpleIdentifierRewriter CreateIdentifierRewriter(SemanticModel model,
+            HashSet<(BaseRewriter rewriter, SyntaxNode originalNode)> nodeSet);
+
+        protected virtual bool ShouldRunTest(string filePath) => true;
+        
+        [Theory]
+        [InlineData("TestData/$1/VisitNode_None.cs", 0)]
+        [InlineData("TestData/$1/VisitNode_Single.cs", 1)]
+        [InlineData("TestData/$1/VisitNode_Multiple.cs", 3)]
+        public virtual void VisitNode_CheckNodeCount(string filePath, int numNodes)
+        {
+            string fullFilePath = filePath.Replace("$1", GetType().Name);
+            if (!ShouldRunTest(fullFilePath)) return;
+
+            string source = File.ReadAllText(fullFilePath);
+            
+            // Create a compilation for the file we are going to rewrite
+            Utils.CreateCSharpCompilation(source, GetType().Name, out _, out CompilationUnitSyntax root, out SemanticModel model);
+            
+            // Create a rewriter
+            HashSet<(BaseRewriter rewriter, SyntaxNode originalNode)> nodeSet = new();
+            SimpleIdentifierRewriter rewriter = CreateIdentifierRewriter(model, nodeSet);
+            
+            // Get all nodes in the file
+            IEnumerable<SyntaxNode> descendantNodes = root.DescendantNodes();
+
+            // Visit the nodes and add them to the nodeSet
+            foreach (SyntaxNode assigmentNode in descendantNodes)
+                rewriter.VisitNode(assigmentNode);
+            
+            Assert.Equal(numNodes, nodeSet.Count);
+        }
+    }
+}
