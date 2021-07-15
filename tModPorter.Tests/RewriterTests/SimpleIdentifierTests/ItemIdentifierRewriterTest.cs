1cfccd110a48f9da0f1c7a346b4270ad13c9a178
Chik3r <ikervaraalarcon@gmail.com>
2021-07-15 16:25:53 -0400

    remove big comment

--- tModPorter.Tests/RewriterTests/SimpleIdentifierTests/ItemIdentifierRewriterTest.cs
+++ tModPorter.Tests/RewriterTests/SimpleIdentifierTests/ItemIdentifierRewriterTest.cs
@@ -9,29 +9,5 @@ namespace tModPorter.Tests.RewriterTests.SimpleIdentifierTests
     {
         protected override SimpleIdentifierRewriter CreateIdentifierRewriter(SemanticModel model,
             HashSet<(BaseRewriter rewriter, SyntaxNode originalNode)> nodeSet) => new ItemIdentifierRewriter(model, null, nodeSet);
-
-        /*
-        [Theory]
-        [InlineData("TestData/ItemIdentifierRewriterData/RewriteNode_Single.cs", "TestData/ItemIdentifierRewriterData/RewriteNode_Single.Fix.cs")]
-        [InlineData("TestData/ItemIdentifierRewriterData/RewriteNode_Multiple.cs", "TestData/ItemIdentifierRewriterData/RewriteNode_Multiple.Fix.cs")]
-        public void RewriteNodeTest(string inputFile, string targetFile)
-        {
-            string source = File.ReadAllText(inputFile);
-            string target = File.ReadAllText(targetFile);
-            Utils.CreateCSharpCompilation(source, "ItemIdentifierTest", out _, out CompilationUnitSyntax root, out SemanticModel model);
-            
-            HashSet<(BaseRewriter rewriter, SyntaxNode originalNode)> nodeSet = new();
-            SimpleIdentifierRewriter rewriter = new ItemIdentifierRewriter(model, null, nodeSet);
-
-            IEnumerable<SyntaxNode> assigmentNodes = root.DescendantNodes();
-
-            foreach (SyntaxNode assigmentNode in assigmentNodes)
-                rewriter.VisitNode(assigmentNode);
-
-            root = root.RewriteMultipleNodes(nodeSet);
-
-            Assert.Equal(target, root.ToFullString());
-        }
-        */
     }
 }
