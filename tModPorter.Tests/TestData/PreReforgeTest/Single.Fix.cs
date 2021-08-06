984ffd8520ba60da8d726cb22e3ce730c0dc3e1b
Chik3r <ikervaraalarcon@gmail.com>
2021-08-05 20:40:10 -0400

    Add tests for PreReforgeRename.cs

---
+++ tModPorter.Tests/TestData/PreReforgeTest/Single.Fix.cs
@@ -0,0 +1,12 @@
+﻿namespace tModPorter.Tests.TestData.PreReforgeTest
+{
+	public class GlobalItem
+	{
+		virtual void PreReforge() { }
+	}
+	
+	public class Single : GlobalItem
+	{
+		override void PreReforge() { }
+	}
+}
