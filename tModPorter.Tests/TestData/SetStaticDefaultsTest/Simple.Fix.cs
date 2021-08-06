6c17a3ade8907e33b779b5904a17e247a12f9889
Chik3r <ikervaraalarcon@gmail.com>
2021-08-05 22:21:29 -0400

    Add tests for `SetStaticDefaultsRename.cs`

---
+++ tModPorter.Tests/TestData/SetStaticDefaultsTest/Simple.Fix.cs
@@ -0,0 +1,22 @@
+﻿namespace tModPorter.Tests.TestData.SetStaticDefaultsTest
+{
+	public class GlobalTile
+	{
+		virtual void SetStaticDefaults() { }
+	}
+	
+	public class ModMount
+	{
+		virtual void SetStaticDefaults() { }
+	}
+	
+	public class SimpleA : GlobalTile
+	{
+		override void SetStaticDefaults() { }
+	}
+	
+	public class SimpleB : ModMount
+	{
+		override void SetStaticDefaults() { }
+	}
+}
---
+++ tModPorter.Tests/TestData/SetStaticDefaultsTest/Simple.cs
@@ -0,0 +1,22 @@
+﻿namespace tModPorter.Tests.TestData.SetStaticDefaultsTest
+{
+	public class GlobalTile
+	{
+		virtual void SetStaticDefaults() { }
+	}
+	
+	public class ModMount
+	{
+		virtual void SetStaticDefaults() { }
+	}
+	
+	public class SimpleA : GlobalTile
+	{
+		override void SetDefaults() { }
+	}
+	
+	public class SimpleB : ModMount
+	{
+		override void SetDefaults() { }
+	}
+}
