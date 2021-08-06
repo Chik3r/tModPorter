1c3548183c923f9fa779bcaa42cbe28baac3f0c1
Chik3r <ikervaraalarcon@gmail.com>
2021-08-05 20:35:36 -0400

    Make Utils.RewriteMultipleNodes also rewrite tokens

--- tModPorter.Tests/Utils.cs
+++ tModPorter.Tests/Utils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -6,9 +7,11 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using tModPorter.Rewriters;
 
 namespace tModPorter.Tests {
-	public static class Utils {
+	public static class Utils
+	{
 		public static CSharpCompilation CreateCSharpCompilation(string source, string assemblyName, out SyntaxTree tree,
-			out CompilationUnitSyntax root, out SemanticModel model) {
+			out CompilationUnitSyntax root, out SemanticModel model)
+		{
 			tree = CSharpSyntaxTree.ParseText(source);
 
 			root = tree.GetCompilationUnitRoot();
@@ -24,14 +27,30 @@ namespace tModPorter.Tests {
 		}
 
 		internal static CompilationUnitSyntax RewriteMultipleNodes(this CompilationUnitSyntax root,
-			IEnumerable<(BaseRewriter rewriter, SyntaxNode originalNode)> nodeSet) {
+			IEnumerable<(BaseRewriter rewriter, SyntaxNode originalNode)>? nodesToRewrite = null,
+			IEnumerable<(BaseRewriter rewriter, SyntaxToken originalToken)>? tokensToRewrite = null)
+		{
+			nodesToRewrite ??= Array.Empty<(BaseRewriter rewriter, SyntaxNode originalNode)>();
 			Dictionary<SyntaxNode, SyntaxNode> nodeDictionary = new();
-			foreach ((BaseRewriter rewriter, SyntaxNode originalNode) in nodeSet) {
+			foreach ((BaseRewriter rewriter, SyntaxNode originalNode) in nodesToRewrite)
+			{
 				SyntaxNode newNode = rewriter.RewriteNode(originalNode);
 				nodeDictionary.Add(originalNode, newNode);
 			}
 
-			return root.ReplaceNodes(nodeDictionary.Keys.AsEnumerable(), (n1, n2) => nodeDictionary[n1]);
+			tokensToRewrite ??= Array.Empty<(BaseRewriter rewriter, SyntaxToken originalToken)>();
+			Dictionary<SyntaxToken, SyntaxToken> tokenDictionary = new();
+			foreach ((BaseRewriter rewriter, SyntaxToken originalToken) in tokensToRewrite)
+			{
+				SyntaxToken newToken = rewriter.RewriteToken(originalToken);
+				tokenDictionary.Add(originalToken, newToken);
+			}
+
+			root = root.ReplaceSyntax(
+				nodes: nodeDictionary.Keys.AsEnumerable(), (original, _) => nodeDictionary[original],
+				tokens: Array.Empty<SyntaxToken>(), (original, _) => tokenDictionary[original],
+				trivia: Array.Empty<SyntaxTrivia>(), (original, _) => original);
+			return root;
 		}
 	}
 }
