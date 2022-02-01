94299aca568afd356236a90ff146052b430f0d3f
Chik3r <ikervaraalarcon@gmail.com>
2022-01-31 23:50:47 -0300

    Generate a test for each test file to rewrite

--- tModPorter.Tests/AutomaticTest.cs
+++ tModPorter.Tests/AutomaticTest.cs
@@ -11,42 +11,58 @@ namespace tModPorter.Tests;
 
 public class AutomaticTest
 {
-    [Test]
-    public void RewriteCode()
+    private Compilation _compilation;
+    private static List<SyntaxTree>? Trees;
+    
+    [OneTimeSetUp]
+    public void Setup()
     {
-        List<string> testFiles = new(Directory.GetFiles("TestData/", "*", SearchOption.AllDirectories).Where(x => !x.Contains(".Fix.cs")));
-        Assert.IsNotEmpty(testFiles);
-
-        List<SyntaxTree> trees = new(testFiles.Count);
-        foreach (string filePath in testFiles) {
-            string text = File.ReadAllText(filePath);
-            trees.Add(CSharpSyntaxTree.ParseText(text, path: filePath));
-        }
+        GetSyntaxTrees();
 
         MetadataReference[] references = { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) };
 
-        CSharpCompilation compilation = CSharpCompilation.Create("TestAssembly", trees, references);
+        _compilation = CSharpCompilation.Create("TestAssembly", Trees, references);
+    }
+    
+    [TestCaseSource(nameof(GetRewritableSyntaxTrees))]
+    public void RewriteCode(SyntaxTree tree)
+    {
+        SemanticModel model = _compilation.GetSemanticModel(tree);
+        SyntaxNode rootNode = tree.GetRoot();
 
-        IEnumerable<SyntaxTree> treesToFix =
-            trees.Where(x => !Path.GetDirectoryName(x.FilePath)!.Replace('\\', '/').Contains("TestData/Common"));
-        foreach (SyntaxTree tree in treesToFix)
-        {
-            SemanticModel model = compilation.GetSemanticModel(tree);
-            SyntaxNode rootNode = tree.GetRoot();
+        MainRewriter rewriter = new(model);
+        rewriter.Visit(rootNode);
+        CompilationUnitSyntax? result = rewriter.RewriteNodes(rootNode) as CompilationUnitSyntax;
+        
+        Assert.NotNull(result);
+        result = rewriter.AddUsingDirectives(result);
 
-            MainRewriter rewriter = new(model);
-            rewriter.Visit(rootNode);
-            CompilationUnitSyntax? result = rewriter.RewriteNodes(rootNode) as CompilationUnitSyntax;
-            
-            Assert.NotNull(result);
-            result = rewriter.AddUsingDirectives(result);
+        string fixedFilePath = Path.ChangeExtension(tree.FilePath, ".Fix.cs");
+        
+        Assert.True(File.Exists(fixedFilePath), $"File '{fixedFilePath}' doesn't exist.");
+        string fixedContent = File.ReadAllText(fixedFilePath);
+        
+        Assert.AreEqual(fixedContent, result.ToFullString());
+    }
 
-            string fixedFilePath = Path.ChangeExtension(tree.FilePath, ".Fix.cs");
-            
-            Assert.True(File.Exists(fixedFilePath), $"File '{fixedFilePath}' doesn't exist.");
-            string fixedContent = File.ReadAllText(fixedFilePath);
-            
-            Assert.AreEqual(fixedContent, result.ToFullString());
+    public static List<SyntaxTree> GetSyntaxTrees()
+    {
+        if (Trees is not null) return Trees;
+        
+        List<string> testFiles = new(Directory.GetFiles("TestData/", "*", SearchOption.AllDirectories).Where(x => !x.Contains(".Fix.cs")));
+        Assert.IsNotEmpty(testFiles);
+
+        Trees = new(testFiles.Count);
+        foreach (string filePath in testFiles) {
+            string text = File.ReadAllText(filePath);
+            Trees.Add(CSharpSyntaxTree.ParseText(text, path: filePath));
         }
+
+        return Trees;
+    }
+
+    public static IEnumerable<SyntaxTree> GetRewritableSyntaxTrees()
+    {
+        return GetSyntaxTrees().Where(x => !Path.GetDirectoryName(x.FilePath)!.Replace('\\', '/').Contains("TestData/Common"));
     }
 }
