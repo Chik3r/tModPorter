b70cdc8cc07c1c77c5915762ce20ad2b8a5c5cbd
Chik3r <ikervaraalarcon@gmail.com>
2022-02-04 20:04:01 -0300

    Move all identifiers rewriters into 2 classes

---
+++ tModPorter/Rewriters/IdentifierRewriters/SimpleIdentifiersRewriter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace tModPorter.Rewriters.IdentifierRewriters
+{
+
+    public class SimpleIdentifiersRewriter : BaseRewriter
+    {
+        private static readonly Dictionary<string, string> IdentifierMap = new()
+        {
+            { "mod", "Mod" },
+            { "item", "Item" },
+            { "npc", "NPC" },
+            { "player", "Player" },
+            { "projectile", "Projectile" },
+        };
+
+        public SimpleIdentifiersRewriter(SemanticModel model, List<string> usingList,
+            HashSet<(BaseRewriter rewriter, SyntaxNode originalNode)> nodesToRewrite,
+            HashSet<(BaseRewriter rewriter, SyntaxToken originalToken)> tokensToRewrite)
+            : base(model, usingList, nodesToRewrite, tokensToRewrite)
+        { }
+
+        public override RewriterType RewriterType => RewriterType.Identifier;
+
+        public override void VisitNode(SyntaxNode node)
+        {
+            if (node is not IdentifierNameSyntax identifier) return;
+
+            // Ignore variable declarations ('var a = 5') or assignments ('a = 5')
+            // This doesn't ignore something like 'mod.field = 5' since 'mod.field' is its own syntax.
+            if (node.Parent is AssignmentExpressionSyntax or VariableDeclaratorSyntax) return;
+
+            if (IdentifierMap.ContainsKey(identifier.ToString()) && !HasSymbol(node, out _))
+                AddNodeToRewrite(identifier);
+        }
+
+        public override SyntaxNode RewriteNode(SyntaxNode node)
+        {
+            if (IdentifierMap.TryGetValue(node.ToString(), out string value))
+                return IdentifierName(value).WithExtraTrivia(node);
+            else
+                return node;
+        }
+    }
+}
---
+++ tModPorter/Rewriters/IdentifierRewriters/TileFieldsRewriter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace tModPorter.Rewriters.IdentifierRewriters;
+
+public class TileFieldsRewriter : BaseRewriter
+{
+    private static readonly Dictionary<string, string> IdentifierMap = new()
+    {
+        { "frameX", "TileFrameX" },
+        { "frameY", "TileFrameY" },
+        { "type", "TileType" },
+        { "wall", "WallType" }
+    };
+    
+    public TileFieldsRewriter(SemanticModel model, List<string> usingList,
+        HashSet<(BaseRewriter rewriter, SyntaxNode originalNode)> nodesToRewrite,
+        HashSet<(BaseRewriter rewriter, SyntaxToken originalToken)> tokensToRewrite)
+        : base(model, usingList, nodesToRewrite, tokensToRewrite)
+    { }
+    
+    public override RewriterType RewriterType => RewriterType.Identifier;
+    
+    public override void VisitNode(SyntaxNode node)
+    {
+        if (node is not IdentifierNameSyntax identifier) return;
+
+        // Only modify member accesses 'a.b'
+        if (node.Parent is not MemberAccessExpressionSyntax memberAccess) return;
+        
+        // Check if the parent node's expression is an array access, example: 'a.b[0].c'
+        if (memberAccess.Expression is ElementAccessExpressionSyntax { Expression: MemberAccessExpressionSyntax nestedMember })
+            memberAccess = nestedMember;
+        
+        string typeName = GetTypeName(memberAccess);
+        if (typeName != "Tilemap" && typeName != "Tile") return;
+
+        if (IdentifierMap.ContainsKey(identifier.ToString()) && !HasSymbol(node, out _))
+            AddNodeToRewrite(identifier);
+    }
+
+    public override SyntaxNode RewriteNode(SyntaxNode node)
+    {
+        if (IdentifierMap.TryGetValue(node.ToString(), out string value))
+            return IdentifierName(value).WithExtraTrivia(node);
+        else
+            return node;
+    }
+}
