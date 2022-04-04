825cb2c08ffba702fb009397b88c8c46dbc5758b
Chik3r <31808958+Chik3r@users.noreply.github.com>
2022-04-03 23:18:34 -0400

    Rename Tile `Dangersense` -> `IsTileDangerous`

---
+++ tModPorter.Tests/TestData/Dangersense.Fix.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace tModPorter.Tests.TestData; 
+
+public class DangersenseTile : ModTile {
+	public override bool IsTileDangerous(int a) {
+		return false;
+	}
+}
+
+public class DangersenseGlobal : GlobalTile {
+	public override bool? IsTileDangerous(int a) {
+		return false;
+	}
+}
---
+++ tModPorter.Tests/TestData/Dangersense.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace tModPorter.Tests.TestData; 
+
+public class DangersenseTile : ModTile {
+	public override bool Dangersense(int a) {
+		return false;
+	}
+}
+
+public class DangersenseGlobal : GlobalTile {
+	public override bool Dangersense(int a) {
+		return false;
+	}
+}
