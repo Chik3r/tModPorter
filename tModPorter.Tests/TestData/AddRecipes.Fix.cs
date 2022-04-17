aa7143893a8df0df1eeb6b865d1026062593ca43
Chik3r <31808958+Chik3r@users.noreply.github.com>
2022-04-17 16:32:32 -0400

    Add test for AddRecipes, not passing yet

---
+++ tModPorter.Tests/TestData/AddRecipes.Fix.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace tModPorter.Tests.TestData; 
+
+public class AddRecipes : ModItem {
+	public override void AddRecipes() {
+		CreateRecipe(1)
+			.AddIngredient(ItemID.Wood, 10)
+			.AddTile(TileID.WorkBenches)
+			.Register();
+
+		Method();
+	}
+	
+	public void Method() { }
+}
---
+++ tModPorter.Tests/TestData/AddRecipes.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace tModPorter.Tests.TestData; 
+
+public class AddRecipes : ModItem {
+	public override void AddRecipes() {
+		ModRecipe recipe = new ModRecipe(mod);
+		recipe.AddIngredient(ItemID.Wood, 10);
+		recipe.AddTile(TileID.WorkBenches);
+		recipe.SetResult(this);
+		recipe.AddRecipe();
+
+		Method();
+	}
+	
+	public void Method() { }
+}
